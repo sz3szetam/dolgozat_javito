782906202425ada39a65510ab27cbbf44d81197a
Szécsi Tamás Dávid <144216869+sz3szetam@users.noreply.github.com>
2023-11-06 11:17:10 +0100

    q
    q

---
+++ javito.js
@@ -0,0 +1,5 @@
+document.getElementById("container").style.backgroundColor="#12406A"
+document.querySelectorAll(".box")[0].style.backgroundColor="#EB8F90"
+document.querySelectorAll(".box")[1].style.backgroundColor="##FFB471"
+document.querySelectorAll(".box")[2].style.backgroundColor="#ADBED2"
+document.body.style.backgroundColor="#CCFFFF"
